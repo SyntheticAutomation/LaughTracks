5133d9669fb5c9b9bbfd0a2f14aa0c6619e2a86e
SyntheticAutomation <mclampet@msudenver.edu>
2018-11-29 11:03:45 -0700

    Adds User_Story_1 location feature test (broken)

--- spec/features/user_sees_comedians_spec.rb
+++ spec/features/user_sees_comedians_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe 'A visitor to our app' do
     expect(page).to have_content(comedian.age)
     expect(page).to have_content(comedian.city)
   end
+
+  it 'should display comedian info in the correct place' do
+    comedian = Comedian.create(name: "Adam Sandler", age: 52, city: "Brooklyn" )
+    visit '/comedians'
+
+    expect(comedian_list).to have_content(comedian.name)
+  end
 end
