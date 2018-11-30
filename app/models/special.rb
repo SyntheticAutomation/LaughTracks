ae67bd6a6f28fee07698fe16e53cf36166161b2a
SyntheticAutomation <mclampet@msudenver.edu>
2018-11-30 11:49:07 -0700

    Adds average_runtime method to special model

--- app/models/special.rb
+++ app/models/special.rb
@@ -1,8 +1,12 @@
 class Special < ActiveRecord::Base
-  
+
   belongs_to :comedian
 
   validates :title, presence: true
   validates :runtime, presence: true
   validates :image, presence: true
+
+  def self.average_runtime
+    average(:runtime).round
+  end
 end
