c4524c615ff8afaecfe409558fc71518232250c0
SyntheticAutomation <mclampet@msudenver.edu>
2018-11-29 22:13:55 -0700

    Adds test for user_story_3

--- spec/features/user_sees_special_titles_spec.rb
+++ spec/features/user_sees_special_titles_spec.rb
@@ -1,8 +1,19 @@
 RSpec.describe 'A visitor to our app' do
   it 'should display the proprietary special titles for every comedian' do
     comedian = Comedian.create(name: "Adam Sandler", age: 52, city: "Brooklyn" )
-    special = Special.create(title: "Adam Sandler: Take Me Away From Here", runtime: 120, image: "google.com/", comedian_id: 1 )
+    special = comedian.specials.create(title: "Adam Sandler: Take Me Away From Here", runtime: 120, image: "google.com/" )
     visit '/comedians'
+
     expect(page).to have_content(special.title)
+  end
+  it 'should display runtime and thumbnail with special titles' do
+    comedian = Comedian.create(name: "Adam Sandler", age: 52, city: "Brooklyn" )
+    special = comedian.specials.create(title: "Adam Sandler: Take Me Away From Here", runtime: 120, image: "google.com/" )
+    visit '/comedians'
+
+    within("#comic-special-#{special.id}") do
+      expect(page).to have_content(special.runtime)
+      expect(page).to have_content(special.image)
+    end
   end
 end
