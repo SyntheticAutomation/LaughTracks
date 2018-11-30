e989e38a777fbe98e1fb1dd078b6799733a464bc
SyntheticAutomation <mclampet@msudenver.edu>
2018-11-30 11:50:25 -0700

    Adds an actual image url to special_titles feature test

--- spec/features/user_sees_special_titles_spec.rb
+++ spec/features/user_sees_special_titles_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe 'A visitor to our app' do
   it 'should display the proprietary special titles for every comedian' do
     comedian = Comedian.create(name: "Adam Sandler", age: 52, city: "Brooklyn" )
-    special = comedian.specials.create(title: "Adam Sandler: Take Me Away From Here", runtime: 120, image: "google.com/" )
+    special = comedian.specials.create(title: "Adam Sandler: Take Me Away From Here", runtime: 120, image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2013/08/01/16/sandler-3.jpg" )
     visit '/comedians'
 
     expect(page).to have_content(special.title)
