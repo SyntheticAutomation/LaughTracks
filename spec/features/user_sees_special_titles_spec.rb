4e13ad4b840e4237a6fa12963013e2c2b7db24b3
SyntheticAutomation <mclampet@msudenver.edu>
2018-11-29 22:37:25 -0700

    Adds actual image of Adam Sandler + correct capybara DSL to find image

--- spec/features/user_sees_special_titles_spec.rb
+++ spec/features/user_sees_special_titles_spec.rb
@@ -8,12 +8,14 @@ RSpec.describe 'A visitor to our app' do
   end
   it 'should display runtime and thumbnail with special titles' do
     comedian = Comedian.create(name: "Adam Sandler", age: 52, city: "Brooklyn" )
-    special = comedian.specials.create(title: "Adam Sandler: Take Me Away From Here", runtime: 120, image: "google.com/" )
+    special = comedian.specials.create(title: "Adam Sandler: Take Me Away From Here", runtime: 120, image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2013/08/01/16/sandler-3.jpg" )
     visit '/comedians'
 
     within("#comic-special-#{special.id}") do
       expect(page).to have_content(special.runtime)
-      expect(page).to have_content(special.image)
+    end
+    within("#special-image") do
+      expect(page).to have_xpath("//img[@src='#{special.image}']")
     end
   end
 end
