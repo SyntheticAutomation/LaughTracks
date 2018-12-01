f8944dac61d84ec299f92cfe7f687cb121cb5642
SyntheticAutomation <mclampet@msudenver.edu>
2018-11-30 18:04:37 -0700

    Fixes a bug with average_runtime

--- spec/features/user_sees_comedians_spec.rb
+++ spec/features/user_sees_comedians_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe 'A visitor to our app' do
   it 'should display a list of comedians and their info' do
     comedian = Comedian.create(name: "Adam Sandler", age: 52, city: "Brooklyn" )
+    special = comedian.specials.create(title: "Adam Sandler: Take Me Away From Here", runtime: 120, image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2013/08/01/16/sandler-3.jpg" )
     visit '/comedians'
 
     expect(page).to have_content(comedian.name)
@@ -10,6 +11,8 @@ RSpec.describe 'A visitor to our app' do
 
   it 'should display comedian info in the correct place' do
     comedian = Comedian.create(name: "Adam Sandler", age: 52, city: "Brooklyn" )
+    special = comedian.specials.create(title: "Adam Sandler: Take Me Away From Here", runtime: 120, image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2013/08/01/16/sandler-3.jpg" )
+
     visit '/comedians'
     within("#comedian-#{comedian.id}") do #targetting specific items within page elements
       expect(page).to have_content(comedian.name)
